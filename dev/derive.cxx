391904cd209d5131ead932b912ac4d153e18c085
Mike <mike@pop-os>
2023-09-29 14:08:28 +0200

    Starting derive.cxx

---
+++ dev/derive.cxx
@@ -0,0 +1,46 @@
+/*
+ * derive.cxx
+ * 
+ * Copyright 2023 Mike <mike@pop-os>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * Derive a vector of combinations using the permutations function
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "../inc/toolbox.hxx"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    // setup the factorisation functions
+    vector<uint64_t> primes;
+    SieveOfEratosthenes(primes, 1000000);
+    
+	vector<uint64_t> factors;
+	vector< vector<uint64_t> > combinations;
+ 	
+	find_factors(primes,12345, factors);
+	
+	return 0;
+}
+
